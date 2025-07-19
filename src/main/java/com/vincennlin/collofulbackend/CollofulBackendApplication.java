82094d5fcba0e7d52c9049b04451dda1afac1c07
vincennlin <vincentagwa@gmail.com>
2025-07-19 23:07:13 +0800

    初始化 Project

---
+++ src/main/java/com/vincennlin/collofulbackend/CollofulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vincennlin.collofulbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollofulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollofulBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincennlin/collofulbackend/CollofulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincennlin.collofulbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollofulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
