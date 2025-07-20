1afd2fb33977bb2afd9e5565039deaf73ba9cfb3
vincennlin <vincentagwa@gmail.com>
2025-07-20 12:06:39 +0800

    將詞性的 Enum 名字改為縮寫 並新增全名

--- src/main/java/com/vincennlin/collofulbackend/payload/word/partofspeech/PartOfSpeech.java
+++ src/main/java/com/vincennlin/collofulbackend/payload/word/partofspeech/PartOfSpeech.java
@@ -7,25 +7,27 @@ import java.util.Set;
 @Getter
 public enum PartOfSpeech {
 
-    NOUN("n.", "名詞", SubPart.COUNTABLE_NOUN, SubPart.UNCOUNTABLE_NOUN),
-    PRONOUN("pron.", "代名詞"),
-    VERB("v.", "動詞", SubPart.TRANSITIVE_VERB, SubPart.INTRANSITIVE_VERB),
-    ADVERB("adv.", "副詞"),
-    ADJECTIVE("adj.", "形容詞"),
-    PREPOSITION("prep.", "介系詞"),
-    CONJUNCTION("conj.", "連接詞"),
-    DETERMINER("det.", "限定詞"),
-    INTERJECTION("interj.", "感嘆詞"),
-    NUMERAL("num.", "數詞"),
-    PHASE("phr.", "片語"),
-    ABBREVIATION("abbr.", "縮寫"),;
+    N("n.", "Noun", "名詞", SubPart.C, SubPart.U),
+    PRON("pron.", "Pronoun", "代名詞"),
+    V("v.", "Verb", "動詞", SubPart.VT, SubPart.VI),
+    ADV("adv.", "Adverb", "副詞"),
+    ADJ("adj.", "Adjective", "形容詞"),
+    PREP("prep.", "Preposition", "介系詞"),
+    CONJ("conj.", "Conjunction", "連接詞"),
+    DET("det.", "Determiner", "限定詞"),
+    INTERJ("interj.", "Interjection", "感嘆詞"),
+    NUM("num.", "Numeral", "數詞"),
+    PHR("phr.", "Phrase", "片語"),
+    ABBR("abbr.", "Abbreviation", "縮寫"),;
 
     private final String abbreviation;
+    private final String full;
     private final String chinese;
     private final Set<SubPart> subParts;
 
-    PartOfSpeech(String abbreviation, String chinese, SubPart... subParts) {
+    PartOfSpeech(String abbreviation, String full, String chinese, SubPart... subParts) {
         this.abbreviation = abbreviation;
+        this.full = full;
         this.chinese = chinese;
         this.subParts = Set.of(subParts);
     }
--- src/main/java/com/vincennlin/collofulbackend/payload/word/partofspeech/SubPart.java
+++ src/main/java/com/vincennlin/collofulbackend/payload/word/partofspeech/SubPart.java
@@ -2,16 +2,18 @@ package com.vincennlin.collofulbackend.payload.word.partofspeech;
 
 public enum SubPart {
 
-    COUNTABLE_NOUN("c.", "可數名詞"),
-    UNCOUNTABLE_NOUN("u.", "不可數名詞"),
-    TRANSITIVE_VERB("vt.", "及物動詞"),
-    INTRANSITIVE_VERB("vi.", "不及物動詞"),;
+    C("c.", "Countable Noun", "可數名詞"),
+    U("u.", "Uncountable Noun", "不可數名詞"),
+    VT("vt.", "Transitive Verb", "及物動詞"),
+    VI("vi.", "Intransitive Verb", "不及物動詞"),;
 
     private final String abbreviation;
+    private final String full;
     private final String chinese;
 
-    SubPart(String abbreviation, String chinese) {
+    SubPart(String abbreviation, String full, String chinese) {
         this.abbreviation = abbreviation;
+        this.full = full;
         this.chinese = chinese;
     }
 }
