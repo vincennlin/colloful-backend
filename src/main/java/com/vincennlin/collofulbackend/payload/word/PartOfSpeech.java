93e2aa9f6a9d0ea4f791ce8895fb69d8ec2138f4
vincennlin <vincentagwa@gmail.com>
2025-11-13 00:58:48 +0800

    在 definition 中新增 'V 動詞' 選項

--- src/main/java/com/vincennlin/collofulbackend/payload/word/PartOfSpeech.java
+++ src/main/java/com/vincennlin/collofulbackend/payload/word/PartOfSpeech.java
@@ -6,17 +6,18 @@ import lombok.Getter;
 public enum PartOfSpeech {
 
     N("n.", "Noun", "名詞"),
-    PRON("pron.", "Pronoun", "代名詞"),
+    ADJ("adj.", "Adjective", "形容詞"),
     VT("vt.", "Transitive Verb", "及物動詞"),
     VI("vi.", "Intransitive Verb", "不及物動詞"),
+    V("v.", "Verb", "動詞"),
     ADV("adv.", "Adverb", "副詞"),
-    ADJ("adj.", "Adjective", "形容詞"),
+    PHR("phr.", "Phrase", "片語"),
+    PRON("pron.", "Pronoun", "代名詞"),
     PREP("prep.", "Preposition", "介系詞"),
     CONJ("conj.", "Conjunction", "連接詞"),
     DET("det.", "Determiner", "限定詞"),
     INTERJ("interj.", "Interjection", "感嘆詞"),
     NUM("num.", "Numeral", "數詞"),
-    PHR("phr.", "Phrase", "片語"),
     ABBR("abbr.", "Abbreviation", "縮寫"),;
 
     private final String abbreviation;
